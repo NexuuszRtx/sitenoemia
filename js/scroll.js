ab68527abe58092191383b242cca1d3b397da4de
NexuuszRtx <limageu4@gmail.com>
2024-03-22 00:07:09 -0300

    noemia5

--- js/scroll.js
+++ js/scroll.js
@@ -21,3 +21,13 @@ sr.reveal('.banner', {duration: 1500
 
 sr.reveal('.cursos', {duration: 1500
 });
+
+sr.reveal('.container', {duration: 1500
+});
+
+
+sr.reveal('.fale-conosco', {duration: 1500
+});
+
+sr.reveal('.noticias', {duration: 1500
+});
